c7574372f486dec5e2e410ba869ffd50831c9239
gaohj <gaohj@163.com>
2020-01-08 16:02:37 +0800

    day3ball

--- day3/ball.js
+++ day3/ball.js
@@ -12,6 +12,17 @@ for(var i=0;i<6;i++){   //随机产生6个随机数
     }
 }
 
+for(var i=0;i<redBall.length;i++){
+    //跟下一个球进行比较
+    for(var j=i+1;j<redBall.length;j++){
+        if(redBall[i]>redBall[j]){
+            var temp = redBall[i];
+            redBall[i] = redBall[j];
+            redBall[j] = temp;
+        }
+    }
+
+}
 
 var blueball = Math.floor(Math.random()*16+1); //随机生成一个 
 //1-16的蓝球
